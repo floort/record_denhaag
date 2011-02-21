7f7ff3a60f75fe9be819224274468f563cb95812
Floor Terra <floort@gmail.com>
2011-02-21 20:34:26 +0100

    Added Groen van Prinstererzaal

--- record_denhaag.sh
+++ record_denhaag.sh
@@ -6,10 +6,17 @@ case "$1" in
 ek)
 	STREAM="http://wm.media51.kpnstreaming.nl/kpn=TweedeKamerL=1eKamer"
 	;;
+gvp)
+	STREAM="http://wm.media51.kpnstreaming.nl/kpn=TweedeKamerL=Prinsterer106"
+	;;
+tk)
+	STREAM="http://wm.media51.kpnstreaming.nl/kpn=TweedeKamerL=Plenair101"
+	;;
 *)
 	echo "Usage: ${0} ek|tk [file.dump]"
-	echo "\tek: Eerste Kamer"
-	echo "\ttk: Tweede kamer"
+	echo "\tek : Eerste Kamer"
+	echo "\tgvp: Groen van Prinstererzaal"
+	echo "\ttk : Tweede kamer"
 esac
 
 if [ ${2} ]; then
