a598502181f63223776e7cea5c83df8a05006cb9
Floor Terra <floort@gmail.com>
2011-02-21 21:01:41 +0100

    Added Troelstrazaal and Thorbeckezaal

--- record_denhaag.sh
+++ record_denhaag.sh
@@ -9,14 +9,23 @@ ek)
 gvp)
 	STREAM="http://wm.media51.kpnstreaming.nl/kpn=TweedeKamerL=Prinsterer106"
 	;;
+tb)
+	STREAM="http://wm.media53.kpnstreaming.nl/kpn=TweedeKamerL=Thorbecke111"
+	;;
 tk)
 	STREAM="http://wm.media51.kpnstreaming.nl/kpn=TweedeKamerL=Plenair101"
 	;;
+ts)
+	STREAM="http://wm.media41.kpnstreaming.nl/kpn=TweedeKamerL=Troelstra116"
+	;;
 *)
-	echo "Usage: ${0} ek|tk [file.dump]"
+	echo "Usage: ${0} ek|gvp|tb|tk|ts [file.dump]"
 	echo "\tek : Eerste Kamer"
 	echo "\tgvp: Groen van Prinstererzaal"
+	echo "\ttb : Thorbeckezaal"
 	echo "\ttk : Tweede kamer"
+	echo "\tts : Troelstrazaal"
+	exit
 esac
 
 if [ ${2} ]; then
